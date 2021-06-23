2b511650a0314828d53fc60fbf652ee00212e6fa
Luan Ogawa <ogawa.luan@gmail.com>
2021-06-22 22:26:51 -0300

    finished authentication, wip: create new room

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,21 @@
-import { useState } from 'react';
-import { createContext } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { auth, firebase } from './services/firebase';
 import { Home } from "./pages/Home";
 import { NewRoom } from './pages/NewRoom';
 
-export const AuthContext = createContext({} as any);
+import { AuthContextProvider } from './contexts/AuthContext';
+import { Room } from './pages/Room';
+
 function App() {
-  const [user, setUser] = useState();
-
-  function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      if (result.user) {
-        const { displayName, photoURL, uid } = result.user;
-
-        if (!displayName || photoURL) {
-          throw new Error('Missing information from Google account.');
-        }
-
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL
-        });
-      }
-    });
-  }
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, signInWithGoogle }}>
-        <Route path="/" component={Home} exact />
-        <Route path="/rooms/new" component={NewRoom} />
-      </AuthContext.Provider>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
