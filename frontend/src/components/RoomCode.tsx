fdaf241f76920dd156a51071e0b79ad7f3b8c1d2
Luan Ogawa <ogawa.luan@gmail.com>
2021-06-24 22:29:09 -0300

    created JoinRoom, Room Code component and RoomPage

---
+++ frontend/src/components/RoomCode.tsx
@@ -0,0 +1,22 @@
+import copyImg from '../assets/images/copy.svg';
+
+import '../styles/room-code.scss';
+
+type RoomCodeProps = {
+  code: string;
+}
+export function RoomCode(props: RoomCodeProps) {
+  function CopyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+  }
+
+  return (
+    <button className="room-code" onClick={CopyRoomCodeToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copiar código" />
+      </div>
+
+      <span>Sala #{props.code}</span>
+    </button>
+  );
+}
